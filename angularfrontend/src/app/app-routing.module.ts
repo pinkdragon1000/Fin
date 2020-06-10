b4287544e07c8b4004d2b86ad6ac887019d3710e
Sita Robinson <sita.robinson@gmail.com>
2020-06-10 07:25:06 -0400

    updating folder structure

--- angularfrontend/src/app/app-routing.module.ts
+++ angularfrontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/page-content/login.component';
 import { ManageAccountsComponent } from './components/page-content/manage-accounts.component';
-import { ProfileComponent } from './components/profile.component';
+import { ProfileComponent } from './components/page-content/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
--- angularfrontend/src/app/app.module.ts
+++ angularfrontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputComponent } from './components/input.component';
+import { InputComponent } from './components/small-components.ts/input.component';
 import { ManageAccountsComponent } from './components/page-content/manage-accounts.component';
 import { LoginComponent } from './components/page-content/login.component';
-import { NavbarComponent } from './components/page-content/navbar.component';
-import { PageTemplateComponent } from './components/pagetemplate.component';
-import { ProfileComponent } from './components/profile.component';
+import { NavbarComponent } from './components/small-components.ts/navbar.component';
+import { PageTemplateComponent } from './components/small-components.ts/pagetemplate.component';
+import { ProfileComponent } from './components/page-content/profile.component';
 
 @NgModule({
   declarations: [
